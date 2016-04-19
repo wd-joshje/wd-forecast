18a77527bbbfa59115fdace418827d12a20babf1
Josh Emerson <josh.j.emerson+wd@gmail.com>
2016-04-19 20:59:39 +0100

    Cache api response

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,27 @@
 var request = require('request');
+var MemoryCache = require('fast-memory-cache');
 
 var router = require('express').Router();
+var cache = new MemoryCache();
+
+var cacheTime = 60; // 1 minute
 
 router.get('/forecast', function(req, res) {
 	var forecastUrl = 'http://api.openweathermap.org/data/2.5/forecast';
 	forecastUrl += '?lat=' + req.query.lat + '&lon=' + req.query.lon;
 	forecastUrl += '&APPID=0a242f2b18dfdabe8ec974dfa3cd7850';
 
+	var forecast = cache.get('forecastUrl');
+
+	if (forecast) {
+		res.set('Content-Type', 'application/json');
+    res.send(body);
+	}
+
   request(forecastUrl, function (error, response, body) {
     if (!error && response.statusCode == 200) {
+    	cache.set(forecastUrl, body, cacheTime);
+    	
     	res.set('Content-Type', 'application/json');
       res.send(body);
     } else {
